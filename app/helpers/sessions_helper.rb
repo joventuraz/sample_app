26bb11dcacf00bb543eee9dd6bc4c8db4a494088
Jose Ventura <jo.venturaz@gmail.com>
2011-11-11 22:22:53 -0300

    Session test

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,9 @@
 module SessionsHelper
 
 	def sign_in(user)
-		cookies.permanent.signed[:remember_token] = [user.id, user.salt]
-		#session[:user_id] = user.id
+		# Cookies or Session
+		#cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+		session[:user_id] = user.id
 		self.current_user = user
 	end
 
@@ -11,8 +12,9 @@ module SessionsHelper
 	end
 
 	def current_user
-		@current_user ||= user_from_remember_token
-		#@current_user ||= User.find(session[:user_id]) if session[:user_id]
+		# Cookies or Session
+		#@current_user ||= user_from_remember_token
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
 
 	def signed_in?
@@ -20,18 +22,19 @@ module SessionsHelper
 	end
 
 	def sign_out
-		cookies.delete(:remember_token)
-		#session[:user_id] = nil
+		# Cookies or Session
+		#cookies.delete(:remember_token)
+		session[:user_id] = nil
 		self.current_user = nil
 	end
 
 	private
+		#Comment out to use session
+		#def user_from_remember_token
+		#	User.authenticate_with_salt(*remember_token)
+		#end
 
-		def user_from_remember_token
-			User.authenticate_with_salt(*remember_token)
-		end
-
-		def remember_token
-			cookies.signed[:remember_token] || [nil, nil]
-		end
+		#def remember_token
+		#	cookies.signed[:remember_token] || [nil, nil]
+		#end
 end
