51f6e97fd9792f0957f2e595eb1cdafbe050075d
Jose Ventura <jo.venturaz@gmail.com>
2011-11-23 00:28:53 -0300

    Added user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_filter :authenticate,   :only => [:index, :edit, :update]
-  before_filter :correct_user,   :only => [:edit, :update]
-  before_filter :admin_user,     :only => :destroy
-  before_filter :signed_in_user, :only => [:new, :create]
+  before_filter :authenticate,   :except => [:show, :new, :create]
+  before_filter :correct_user,   :only   => [:edit, :update]
+  before_filter :admin_user,     :only   => :destroy
+  before_filter :signed_in_user, :only   => [:new, :create]
 
 	def show
 		@user = User.find(params[:id])
@@ -70,6 +70,20 @@ class UsersController < ApplicationController
 	    flash[:success] = "User removed."
     end
 	  redirect_to users_path
+  end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
   end
 	
 	private
